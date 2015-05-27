c454224999b5f4ed82b261af5adbe83b66040f63
Miaou <miaou.pbl@gmail.com>
2015-05-28 00:49:52 +0200

    Changement du component pour un fart moins predictible

--- scripts/components/farter.lua
+++ scripts/components/farter.lua
@@ -3,17 +3,52 @@
 local Farter = Class(function(self, inst)
 	self.inst = inst
 
-	-- wat'mil moyens de se faire appeler
-	-- Commençons simple,
-	self.task = self.inst:DoPeriodicTask(10, function() self:DoRandomFart() end)
-	-- Pour faire plus compliquer, on peut faire des events listener et enregistrer la classe dans inst:StartUpdatingComponent (cf components/wisecracker), et il faudra générer l'event
+	self.minTime = 5
+	self.delta = 30
+	self.task = nil
+	--self:Start()
 end)
 
-function Farter:DoRandomFart()
-	if math.random() < 0.9 then
-		-- On peut imaginer une petite animation, et il faudra déplacer tout ça dans un prefab dans ce cas (cf prefabs/flies)
-		self.inst.SoundEmitter:PlaySound("farts/characters/pab/fart")
+-- Fonction à appeler régulièrement
+function Farter:DoFart()
+	if self.inst.fart ~= nil then
+		self.inst.fart:Remove()
 	end
+	self.inst.fart = SpawnPrefab("fartcloud") --self.inst:SpawnChild("fartcloud") -- Not SpawnChild because the child follows the leader...
+	if self.inst.fart ~= nil then
+		local x, y, z = self.inst.Transform:GetWorldPosition()
+		self.inst.fart.Transform:SetPosition(x,y+.4,z)
+		self.inst.fart.Transform:SetRotation(self.inst.Transform:GetRotation())
+		--print('Heading angle '..self.inst.Transform:GetRotation()) -- Pas pratique
+	else
+		print('Fartcloud not spawned')
+	end
+	self:Start()
+end
+
+function Farter:Start()
+	if self.task then
+		self:Stop()
+	end
+
+	-- wat'mil moyens de se faire appeler
+	-- Commençons simple,
+	self.task = self.inst:DoTaskInTime(self.minTime+math.random()*self.delta,
+									   function() self:DoFart() end)
+	-- Pour faire plus compliquer, on peut faire des events listener et enregistrer la classe dans inst:StartUpdatingComponent (cf components/wisecracker), et il faudra générer l'event
+end
+
+function Farter:Stop()
+	if self.task then
+		self.task:Cancel()
+		self.task = nil
+	end
+end
+
+function Farter:SetTimes(minTime, maxTime)
+	self.minTime = minTime
+	self.delta = maxTime-minTime
+	if self.task then self:Start() end
 end
 
 return Farter
--- scripts/prefabs/pab.lua
+++ scripts/prefabs/pab.lua
@@ -53,8 +53,12 @@ end
 
 
 local function master_postinit(inst)
-    inst:AddComponent("farter")
-    --inst.components.farter
+    inst:AddComponent("farter") -- Maintenant que j'ai fait le prefab fartcloud, ça vaudrait le coup de prendre un periodicspawner  (cf prefab/beefalo)
+    -- Non, pas de periodicspawner, parce que ça sert à ne pas surcharger une zone en entités, ce n'est pas ce qu'on veut ici.
+    --inst.components.farter:SetTimes(2,3) -- 2 + rand()*3
+    inst.components.farter:Start()
+    -- TODO: surveiller la transformation en ghost et farter:Stop()
+    -- TODO: surveiller l'état de combat : plus de pets en combat ??? ;-)
 end
 
 
